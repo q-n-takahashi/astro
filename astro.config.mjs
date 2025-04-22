556eb9a2fd5cec111b06540650ffb65057f77c53
n-takahashi <n-takahashi@q-co.jp>
2025-04-22 17:10:08 +0900

    Update index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jocular-gelato-58a66b.netlify.app",
   adapter: netlify()
 });
